11b8fd068de5bbc9308a5f6fec699f11a143c9ad
SyedaKa <ksyeda2003@gmail.com>
2024-08-24 15:07:32 +0400

    corrected username

--- dashboard.php
+++ dashboard.php
@@ -65,7 +65,7 @@
         });
         const baseURL = window.location.origin;
         function showModal(h_id,is_team) {
-            <?php if (isset($_SESSION['user_isadmin']) && $_SESSION['user_isadmin']==3): ?>
+            <?php if (isset($_SESSION['user_isadmin']) && $_SESSION['user_isadmin']==1): ?>
                 const eventLink =`${baseURL}/code-battle/events/eventreg.php?H=${h_id}&T=${is_team}&L=yes`;
                 document.getElementById("event-link").textContent=eventLink ;
                 document.getElementById("register-button").onclick= function() {
@@ -113,9 +113,9 @@
         </div>
         <div class="welcome-container">
             <div class="welcome">
-                <?php if (isset($_SESSION['user_id']) || isset($_SESSION['user_isadmin'])):?>
+                <!--<?php if (isset($_SESSION['user_id']) || isset($_SESSION['user_isadmin'])):?>
                     <h1>Welcome, <span class="username"><?php echo $user['username']; ?></span></h1>
-                <?php endif; ?>
+                <?php endif; ?>-->
             </div>
         </div>
         <div class="content">
